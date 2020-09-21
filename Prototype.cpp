aa63da169017741486117f7befe9c18bb9acb2e5
Jeyyueyen <71243482+Jeyyueyen@users.noreply.github.com>
2020-09-21 22:32:56 +0800

    Add files via upload

---
+++ Prototype.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+main(){
+
+
+string name,course;
+int age;
+
+cout<<"Enter Your Full Name: ";
+cin>>name;
+cout<<"Enter Your Age: ";
+cin>>age;
+cout<<"Enter Your Course: ";
+cin>>course;
+
+cout<<name<<" a "<<course<<" Student"<<" Is "<<age<<" years old"<<endl;
+
+getch();
+}
